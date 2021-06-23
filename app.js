5b8f457f7d21949cca9d23aa5ea5d1cb55122f48
MarioG8 <golikmariusz7@gmail.com>
2021-06-23 10:23:12 +0200

    ready for heroku

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// View engine setup
+app.engine("handlebars", exphbs({ defaultLayout: false }));
+app.set("view engine", "handlebars");
+
+// Static folder
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("form", { layout: false });
+});
+
+app.post("/send", (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>FullName: ${req.body.fullname}</li>
+      <li>Email: ${req.body.email}</>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.EMAIL_USER, // generated ethereal user
+        pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Nodemailer Contact 📧.." <EMAIL_USER>', // sender address
+      to: "vipmario@wp.pl", // list of receivers
+      subject: "Node Contact Request✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: output, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
+
+  res.render("form", { msg: "Email has been sent!" });
+  main().catch(console.error);
+});
+
+app.listen(port, () =>
+  console.log(`Server is running and listening at http://localhost:${port}`)
+);
